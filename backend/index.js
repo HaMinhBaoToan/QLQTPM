b26e345429651c255613c2ecdbf4265ffbfa96db
haminhbaotoan <hmbt93@gmail.com>
2020-12-04 13:08:18 +0700

    add docx

--- backend/index.js
+++ backend/index.js
@@ -46,5 +46,5 @@ app.use(function (err, req, res, next) {
 
 
 app.listen(PORT, function () {
-  console.log(`Server is running on Port: ${PORT}`);
+  console.log(`Server is running on Port: http://localhost:${PORT}`);
 });
