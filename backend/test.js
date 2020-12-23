57be7a67222fb487ac21427295428d87b394362f
hmbt93@gmail.com <hmbt93@gmail.com>
2020-12-23 21:38:52 +0700

    no message

--- backend/test.js
+++ backend/test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect
 const server = require('./index.js');
 
 describe('test', () => {
-  it('should return a string', () => {
+  it('should return a string wqeqweqweq', () => {
     expect('ci with travis').to.equal('ci with travis');
   });
 });
