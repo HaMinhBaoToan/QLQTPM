7fc608535bd839e585b0611a272db6ec8c0d0cda
Thanh Thuy <thanhthuya77@gmail.com>
2020-12-25 23:41:31 +0700

    Use Row and Col of Grid

--- frontend/src/containers/Orders/index.js
+++ frontend/src/containers/Orders/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Products from './component/Products';
+import { Row, Col } from 'antd';
 import './orders.scss';
 
 const Orders = () => {
@@ -15,7 +16,12 @@ const Orders = () => {
 
   return (
     <div className="products loading">
-      <Products productsList={products} />
+      <Row>
+        <Col span={18}>
+          <Products productsList={products} />
+        </Col>
+        <Col span={6}>ahihi</Col>
+      </Row>
     </div>
   );
 };
