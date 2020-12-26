ef4568c6a000cde8d673247b28d5ae85b39d05c9
tommy.vinh.le <tommy.vinh.le@somotsoftvn.com>
2020-12-27 00:05:50 +0700

    update CRUD products

--- backend/server/router/products.route.js
+++ backend/server/router/products.route.js
@@ -8,6 +8,7 @@ router.get('/', async function (req, res) {
   const list = await productsModel.all();
   res.json(list);
 })
+
 router.get('/:id', async function (req, res) {
   const id = req.params.id || 0;
   const product = await productsModel.single(id);
@@ -17,5 +18,39 @@ router.get('/:id', async function (req, res) {
 
   res.json(product);
 });
+
+
+router.post('/', async function(req, res){
+
+  const product = req.body;
+  const id_list = await productsModel.add(product);
+  order.Product_ID = id_list[0];
+
+  res.status(201).json(product);     // created
+})
+
+router.delete('/:id', async function(req, res){
+  const id = req.params.id || 0;
+  if(id === 0){
+      return res.status(304).end()
+  }
+
+  await productsModel.delete(id);
+
+  res.json({
+      message: "delete success"
+  });
+})
+
+router.put('/:id', async function(req, res){
+  const id = req.params.id;
+  const product = req.body ;
+
+  await productsModel.update(id, product);
+
+  res.json({
+      message: "update success"
+  });
+})
 
 module.exports = router;
