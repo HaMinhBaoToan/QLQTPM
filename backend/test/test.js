2c2cefb62d0fb1791b85d95a80440119ca5914ca
hmbt93@gmail.com <hmbt93@gmail.com>
2021-01-04 23:04:28 +0700

    no message

--- backend/test/test.js
+++ backend/test/test.js
@@ -10,18 +10,24 @@ let should = chai.should();
 chai.use(chaiHttp);
 //Our parent block
 // const productsModel = require("../server/models/order-detail.model");
-describe("GET /api/order-detail", () => {
-  it("respond with list Product", (done) => {
-    // const list = await productsModel.single(1);
-    // chai.request(app).get("/api/order-detail/1").expect(list[0], done);
-    chai
-      .request(server)
-      .get("/api/order-detail")
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a("array");
-        res.body.length.should.be.eql(29); // fixme :)
-        done();
-      });
+
+describe("Details", () => {
+  beforeEach((done) => {
+    done();
+  });
+  describe("GET /api/order-detail", () => {
+    it("respond with list Product", (done) => {
+      // const list = await productsModel.single(1);
+      // chai.request(app).get("/api/order-detail/1").expect(list[0], done);
+      chai
+        .request(server)
+        .get("/")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("Object");
+          res.body.should.be.eql({"message":"Welcome to Project Support"}); // fixme :)
+          done();
+        });
+    });
   });
 });
