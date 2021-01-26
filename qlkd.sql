7a779bed3c0b7c4a0ea36ad735956f80f37c0c52
hmbt93@gmail.com <hmbt93@gmail.com>
2021-01-26 09:58:29 +0700

    up database

--- qlkd.sql
+++ qlkd.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 02/01/2021 10:48:32
+ Date: 26/01/2021 09:57:35
 */
 
 SET NAMES utf8mb4;
@@ -28,7 +28,7 @@ CREATE TABLE `branchs`  (
   `Branch_Mobile` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `Branch_IsActive` bit(1) NOT NULL DEFAULT b'1',
   PRIMARY KEY (`Branch_ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of branchs
@@ -81,35 +81,38 @@ CREATE TABLE `goods`  (
   PRIMARY KEY (`Goods_ID`) USING BTREE,
   INDEX `fk_goods_suppliers_idx`(`Goods_SupplierID`) USING BTREE,
   CONSTRAINT `fk_goods_suppliers` FOREIGN KEY (`Goods_SupplierID`) REFERENCES `suppliers` (`Supplier_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 81 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
-INSERT INTO `goods` VALUES (1, 'Hạt cà phê Espresso', '2020-12-09 10:36:04', 3, 'kg', 1004, 95000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (2, 'Hạt cà phê Arabica', '2020-12-09 10:36:04', 3, 'kg', 1004, 180000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (3, 'Hạt cà phê Robusta', '2020-12-09 10:36:04', 3, 'kg', 1004, 100000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (4, 'Đường trắng', '2020-12-09 10:36:04', 6, 'kg', 1004, 12000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (5, 'Đường nâu', '2020-12-09 10:36:04', 2, 'kg', 1004, 27000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (6, 'Sữa đặc', '2020-12-09 10:36:04', 12, 'Hộp', 1004, 56000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (7, 'Sữa tươi', '2020-12-09 10:36:04', 15, 'Hộp', 1004, 35000, NULL, '2020-11-01 10:36:04', '2021-12-01 10:36:04');
-INSERT INTO `goods` VALUES (8, 'Trà túi lọc Cozy', '2020-12-09 10:36:04', 4, 'Hộp', 1004, 30000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (9, 'Trà túi lọc Lipton', '2020-12-09 10:36:04', 4, 'Hộp', 1005, 33000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (10, 'Trà Đen', '2020-12-09 10:36:04', 3, 'kg', 1005, 135000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (11, 'Trà Ô Long', '2020-12-09 10:36:04', 3, 'kg', 1005, 300000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (12, 'Hồng Trà', '2020-12-09 10:36:04', 3, 'kg', 1005, 135000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (13, 'Bột pudding socola', '2020-12-09 10:36:04', 7, 'kg', 1005, 145000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (14, 'Bột cacao', '2020-12-09 10:36:04', 4, 'kg', 1005, 16000, NULL, '2020-11-10 10:36:04', '2021-12-01 10:36:04');
-INSERT INTO `goods` VALUES (15, 'Bột trà matcha', '2020-12-09 10:36:04', 2, 'kg', 1005, 300000, NULL, '2020-11-10 10:36:04', '2021-12-01 10:36:04');
-INSERT INTO `goods` VALUES (16, 'Bột kem sữa', '2020-12-09 10:36:04', 3, 'kg', 1005, 215000, NULL, '2020-11-10 10:36:04', '2021-12-01 10:36:04');
-INSERT INTO `goods` VALUES (17, 'Trân châu đen', '2020-12-09 10:36:04', 5, 'Hộp', 1005, 40000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (18, 'Hạt chia Đen', '2020-12-09 10:36:04', 1, 'kg', 1005, 200000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (19, 'Thạch đào', '2020-12-09 10:36:04', 5, 'Hộp', 1006, 60000, NULL, '2020-12-01 10:36:04', '2021-06-01 10:36:04');
-INSERT INTO `goods` VALUES (20, 'Thạch trái cây', '2020-12-09 10:36:04', 5, 'Hộp', 1006, 60000, NULL, '2020-12-01 10:36:04', '2021-06-01 10:36:04');
-INSERT INTO `goods` VALUES (21, 'Đào ngâm Alcurnia', '2020-12-09 10:36:04', 4, 'Hộp', 1006, 60000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (22, 'Vải đóng hộp EL GRECO', '2020-12-09 10:36:04', 4, 'Hộp', 1006, 33000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (23, 'Siro hương dâu Heston', '2020-12-09 10:36:04', 2, 'Chai', 1006, 160000, NULL, NULL, NULL);
-INSERT INTO `goods` VALUES (24, 'Socola', '2020-12-09 10:36:04', 2, 'Chai', 1006, 75000, NULL, NULL, NULL);
+INSERT INTO `goods` VALUES (1, 'Hạt cà phê Espresso', '2020-10-12 19:08:24', 3, 'kg', 1004, 95000, NULL, '2020-09-17 13:22:15', '2021-01-17 07:27:40');
+INSERT INTO `goods` VALUES (2, 'Hạt cà phê Arabica', '2020-10-13 05:33:31', 3, 'kg', 1004, 180000, NULL, '2020-09-17 21:13:41', '2022-09-20 15:00:24');
+INSERT INTO `goods` VALUES (3, 'Hạt cà phê Robusta', '2020-10-15 01:00:19', 3, 'kg', 1004, 100000, NULL, '2020-09-22 10:17:35', '2022-10-13 09:26:48');
+INSERT INTO `goods` VALUES (4, 'Đường trắng', '2020-10-15 20:58:40', 6, 'kg', 1004, 12000, NULL, '2020-09-23 08:30:47', '2022-10-16 17:44:57');
+INSERT INTO `goods` VALUES (5, 'Đường nâu', '2020-10-18 10:28:29', 2, 'kg', 1004, 27000, NULL, '2020-09-27 14:02:40', '2022-11-05 23:34:02');
+INSERT INTO `goods` VALUES (6, 'Sữa đặc', '2020-10-22 10:49:16', 12, 'Hộp', 1004, 56000, NULL, '2020-09-28 18:27:03', '2020-11-08 20:07:11');
+INSERT INTO `goods` VALUES (7, 'Sữa tươi', '2020-10-26 23:04:56', 15, 'Hộp', 1004, 35000, NULL, '2020-09-30 05:26:28', '2022-12-22 23:08:58');
+INSERT INTO `goods` VALUES (8, 'Trà túi lọc Cozy', '2020-10-28 21:24:31', 4, 'Hộp', 1004, 30000, NULL, '2020-10-02 05:37:48', '2021-01-28 20:31:01');
+INSERT INTO `goods` VALUES (9, 'Trà túi lọc Lipton', '2020-11-02 12:30:54', 4, 'Hộp', 1005, 33000, NULL, '2020-10-03 15:54:47', '2023-02-01 11:36:12');
+INSERT INTO `goods` VALUES (10, 'Trà Đen', '2020-11-05 01:14:34', 3, 'kg', 1005, 135000, NULL, '2020-10-03 18:41:04', '2021-03-08 12:27:17');
+INSERT INTO `goods` VALUES (11, 'Trà Ô Long', '2020-11-07 00:23:39', 3, 'kg', 1005, 300000, NULL, '2020-10-05 04:19:22', '2023-03-23 12:01:43');
+INSERT INTO `goods` VALUES (12, 'Hồng Trà', '2020-11-10 17:34:35', 3, 'kg', 1005, 135000, NULL, '2020-10-07 09:46:45', '2023-04-19 19:03:18');
+INSERT INTO `goods` VALUES (13, 'Bột pudding socola', '2020-11-16 01:19:31', 7, 'kg', 1005, 145000, NULL, '2020-10-09 15:36:14', '2023-04-25 07:04:49');
+INSERT INTO `goods` VALUES (14, 'Bột cacao', '2020-11-18 17:33:38', 4, 'kg', 1005, 16000, NULL, '2020-10-18 07:43:47', '2023-05-15 01:23:41');
+INSERT INTO `goods` VALUES (15, 'Bột trà matcha', '2020-11-26 15:21:09', 2, 'kg', 1005, 300000, NULL, '2020-10-19 03:52:59', '2023-05-27 14:27:05');
+INSERT INTO `goods` VALUES (16, 'Bột kem sữa', '2020-11-27 08:42:38', 3, 'kg', 1005, 215000, NULL, '2020-10-26 01:54:18', '2023-06-05 20:57:06');
+INSERT INTO `goods` VALUES (17, 'Trân châu đen', '2020-11-28 23:29:51', 5, 'Hộp', 1005, 40000, NULL, '2020-10-26 04:14:21', '2023-06-12 14:10:20');
+INSERT INTO `goods` VALUES (18, 'Hạt chia Đen', '2020-12-10 08:01:37', 1, 'kg', 1005, 200000, NULL, '2020-10-31 17:09:45', '2023-06-26 10:07:31');
+INSERT INTO `goods` VALUES (19, 'Thạch đào', '2020-12-14 08:48:01', 5, 'Hộp', 1006, 60000, NULL, '2020-11-02 03:38:24', '2023-07-02 01:06:04');
+INSERT INTO `goods` VALUES (20, 'Thạch trái cây', '2020-12-21 11:43:44', 5, 'Hộp', 1006, 60000, NULL, '2020-11-07 20:09:23', '2023-07-15 08:28:53');
+INSERT INTO `goods` VALUES (21, 'Đào ngâm Alcurnia', '2020-12-22 13:23:13', 4, 'Hộp', 1006, 60000, NULL, '2020-11-09 20:34:19', '2023-07-27 08:20:26');
+INSERT INTO `goods` VALUES (22, 'Vải đóng hộp EL GRECO', '2020-12-22 13:37:50', 4, 'Hộp', 1006, 33000, NULL, '2020-11-11 15:19:55', '2023-08-03 12:53:22');
+INSERT INTO `goods` VALUES (23, 'Siro hương dâu Heston', '2020-12-24 22:50:17', 2, 'Chai', 1006, 160000, NULL, '2020-11-15 09:01:27', '2023-08-11 13:28:26');
+INSERT INTO `goods` VALUES (24, 'Socola', '2020-12-28 19:40:48', 2, 'Chai', 1006, 75000, NULL, '2020-11-16 03:56:55', '2023-08-12 07:20:05');
+INSERT INTO `goods` VALUES (73, 'Chai nước 5 lít', '2021-01-19 20:50:44', 3, 'chai', 1004, 20000, NULL, '2021-01-20 00:00:00', '2021-02-19 00:00:00');
+INSERT INTO `goods` VALUES (79, 'Ly Nước 1000ML', '2021-01-21 02:49:03', 500, 'Chiếc', 1004, 1700, NULL, '2023-02-04 00:00:00', '2023-02-04 00:00:00');
+INSERT INTO `goods` VALUES (80, 'Ly Nước 500ML', '2021-01-21 04:16:20', 60, 'Chiếc', 1005, 1850, NULL, '2024-02-02 00:00:00', '2024-02-09 00:00:00');
 
 -- ----------------------------
 -- Table structure for languages
@@ -152,7 +155,7 @@ CREATE TABLE `orders`  (
   CONSTRAINT `fk_orders_branchs` FOREIGN KEY (`Order_BranchID`) REFERENCES `branchs` (`Branch_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `fk_orders_users_C` FOREIGN KEY (`Order_CustomerID`) REFERENCES `users` (`User_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `fk_orders_users_E` FOREIGN KEY (`Order_EmployeesID`) REFERENCES `users` (`User_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 117 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 119 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of orders
@@ -176,6 +179,7 @@ INSERT INTO `orders` VALUES (114, NULL, 1, '0327247666', 'Hà Minh Bảo Toàn',
 INSERT INTO `orders` VALUES (115, NULL, 1, '123', '123', 'Done', '2021-01-01 13:26:05', 0, 1, 1, 19000);
 INSERT INTO `orders` VALUES (116, NULL, 1, '1231', '123q', 'Done', '2021-01-01 13:26:11', 0, 1, 1, 19000);
 INSERT INTO `orders` VALUES (117, NULL, 1, '123', '123', 'Done', '2021-01-01 14:45:05', 0, 1, 2, 38000);
+INSERT INTO `orders` VALUES (118, NULL, 1, '123', '123', 'Done', '2021-01-07 18:01:38', 0, 1, 3, 67000);
 
 -- ----------------------------
 -- Table structure for orders_details
@@ -188,7 +192,7 @@ CREATE TABLE `orders_details`  (
   `Quantity` int(0) NULL DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE,
   INDEX `fk_orders_details_products_idx`(`ProductID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 41 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 46 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of orders_details
@@ -222,6 +226,9 @@ INSERT INTO `orders_details` VALUES (39, 115, 59, 1);
 INSERT INTO `orders_details` VALUES (40, 116, 59, 1);
 INSERT INTO `orders_details` VALUES (41, 117, 59, 1);
 INSERT INTO `orders_details` VALUES (42, 117, 60, 1);
+INSERT INTO `orders_details` VALUES (43, 118, 59, 1);
+INSERT INTO `orders_details` VALUES (44, 118, 60, 1);
+INSERT INTO `orders_details` VALUES (45, 118, 61, 1);
 
 -- ----------------------------
 -- Table structure for products
@@ -347,7 +354,7 @@ CREATE TABLE `suppliers`  (
   `Supplier_Homepage` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `Supplier_Description` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`Supplier_ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1006 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1007 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of suppliers
@@ -355,6 +362,33 @@ CREATE TABLE `suppliers`  (
 INSERT INTO `suppliers` VALUES (1004, 'Nhà Cung Cấp Mai Anh', 'Nguyễn Thị Mai Anh', NULL, '877 Cách Mạng Tháng Tám, Chánh Nghĩa, Thủ Dầu Một, Bình Dương', '079841452', NULL, NULL);
 INSERT INTO `suppliers` VALUES (1005, 'Nhà Cung Cấp Chín Hưng', 'Trần Trung Hưng', NULL, 'Số 400 Đường 30/4, Chánh Nghĩa, Thủ Dầu Một, Bình Dương', '0932123652', NULL, NULL);
 INSERT INTO `suppliers` VALUES (1006, 'Nhà Cung Cấp Tạp hóa Mười', 'Lê Thị Muòi', NULL, '106 Đại lộ Bình Dương, Phú Hoà, Thủ Dầu Một, Bình Dương', '093340425', NULL, NULL);
+
+-- ----------------------------
+-- Table structure for useds
+-- ----------------------------
+DROP TABLE IF EXISTS `useds`;
+CREATE TABLE `useds`  (
+  `Used_ID` int(0) NOT NULL AUTO_INCREMENT,
+  `Used_Goods_ID` int(0) NOT NULL,
+  `Used_Quantity` int(0) NOT NULL,
+  `Used_Description` varbinary(255) NULL DEFAULT NULL,
+  `Used_Date` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`Used_ID`) USING BTREE,
+  INDEX `Used_Goods_ID`(`Used_Goods_ID`) USING BTREE,
+  CONSTRAINT `useds_ibfk_1` FOREIGN KEY (`Used_Goods_ID`) REFERENCES `goods` (`Goods_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of useds
+-- ----------------------------
+INSERT INTO `useds` VALUES (1, 6, 1, NULL, '2020-10-10 19:06:20');
+INSERT INTO `useds` VALUES (2, 7, 3, NULL, '2021-01-26 19:06:39');
+INSERT INTO `useds` VALUES (3, 5, 1, NULL, '2021-01-05 19:06:42');
+INSERT INTO `useds` VALUES (15, 79, 500, NULL, '2021-01-21 02:50:00');
+INSERT INTO `useds` VALUES (17, 73, 1, NULL, '2021-01-21 02:59:39');
+INSERT INTO `useds` VALUES (18, 73, 2, NULL, '2021-01-21 02:59:44');
+INSERT INTO `useds` VALUES (19, 80, 35, NULL, '2021-01-21 04:16:43');
+INSERT INTO `useds` VALUES (20, 80, 25, NULL, '2021-01-21 04:16:51');
 
 -- ----------------------------
 -- Table structure for users
