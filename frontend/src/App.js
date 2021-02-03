0002170c024a8ccb9a323f8ca7f851085332c475
hmbt93@gmail.com <hmbt93@gmail.com>
2021-02-03 12:44:15 +0700

    login + logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 import "./App.scss";
 import "./assets/css/global.scss";
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Sidebar from "./components/Sidebar/index";
 import Dashboard from "./containers/DashBoard";
 import Orders from "./containers/Orders";
@@ -15,20 +20,42 @@ import AntIcon, {
 } from "@ant-design/icons";
 import { AppContext } from "./utils/AppContext";
 import Products from "./containers/Products";
-import Cusomters from './containers/Customers';
-import Employees from './containers/Employees';
-import Login from './components/Login/index';
+import Cusomters from "./containers/Customers";
+import Employees from "./containers/Employees";
+import Login from "./components/Login/index";
 
 const { Header, Sider, Content } = Layout;
 
 function App() {
   const [collapsed, setCollapsed] = useState(false);
-  const [accessToken, setAccessToken] = useState();
+  const [accessToken, setAccessToken] = useState(false);
 
   const handleCollapsed = () => setCollapsed(!collapsed);
-
-  if(!accessToken) {
-    return <Login setAccessToken={setAccessToken} />
+  useEffect(() => {
+    const tokenString = localStorage.getItem("QuanLyKinhDoanh_Token");
+    if (tokenString) {
+      setAccessToken(true);
+    }
+  }, [accessToken]);
+  if (!accessToken) {
+    return (
+      // <Router>
+      // <Switch>
+      // <Redirect push to="/login"  />
+      //   <Route exact path='/login'  >
+      //     <Login setAccessToken={setAccessToken} />;
+      //   </Route>
+      //   </Switch>
+      // </Router>
+      <Router>
+      <Switch>
+        <Redirect exact from="/" to="/login" />
+        <Route path="/login">
+          <Login setAccessToken={setAccessToken} />;
+        </Route>
+      </Switch>
+      </Router>
+    );
   }
 
   return (
@@ -38,7 +65,7 @@ function App() {
           <Layout>
             <Sider trigger={null} collapsible collapsed={collapsed}>
               <div className="logo" />
-              <Sidebar sidebar={collapsed} showSidebar={handleCollapsed} />
+              <Sidebar sidebar={collapsed} showSidebar={handleCollapsed}  setAccessToken={setAccessToken}/>
             </Sider>
             <Layout className="site-layout">
               <Header className="site-layout-background">
@@ -54,7 +81,6 @@ function App() {
               </Header>
               <Content>
                 <Switch>
-                  <Route path='/login' component={Login} />
                   <Route path="/orders">
                     <Orders />
                   </Route>
@@ -65,10 +91,10 @@ function App() {
                     <Products />
                   </Route>
                   <Route path="/customers">
-                    <Cusomters/>
+                    <Cusomters />
                   </Route>
                   <Route path="/employees">
-                    <Employees/>
+                    <Employees />
                   </Route>
                   <Route path="/reports">
                     <h3>This page is updating !</h3>
